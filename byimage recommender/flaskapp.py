a2405940dfd983289eadbc66620af3b49a9ac490
Bartosz Wlazło <bart.wlazlo@gmail.com>
2019-05-26 12:54:39 +0200

    1.5 HOME PAGE list of product generation

--- byimage recommender/flaskapp.py
+++ byimage recommender/flaskapp.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import sys
+import os
+
+sys.path.append("..")  # Adds higher directory to python modules path.
+input_path = './static/resources/img/test_images'
+all_paths = []
+
+def GET_ALL():
+    for file in os.listdir(input_path):
+        filename = os.fsdecode(file)
+        all_paths.append(filename)
+    return all_paths
+
+datapaths=GET_ALL()
 
 app = Flask(__name__, template_folder="templates")
-
 @app.route('/')
 def home():
     """Landing page."""
-    return render_template('/index.html', title="Lame Site")
+    return render_template('/index.html', title="Lame Site", allpaths=datapaths)
+
+
+
 
 """from flask import Flask
 app = Flask(__name__)
--- byimage recommender/get_all_paths.py
+++ byimage recommender/get_all_paths.py
@@ -1,14 +1,12 @@
 import sys
 import os
 sys.path.append("..")  # Adds higher directory to python modules path.
-from PIL import Image
-
 input_path = './static/resources/img/test_images'
-
 all_paths = []
-for file in os.listdir(input_path):
-    filename = os.fsdecode(file)
-    all_paths.append(filename)
-
-
-print(filename)
+def GET_ALL():
+    for file in os.listdir(input_path):
+        filename = os.fsdecode(file)
+        all_paths.append("./static/resources/img/test_images/"+filename)
+    return all_paths
+aa=GET_ALL()
+print(aa)
